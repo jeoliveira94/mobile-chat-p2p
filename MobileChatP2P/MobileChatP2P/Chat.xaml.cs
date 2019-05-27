aeee3cc9511739cbb6993179ebe98b43ecc7a897
José Eduardo de Souza Oliveira <eduardo.jeso@gmail.com>
2019-05-27 18:43:05 -0300

    Chat view, mensagem view

--- MobileChatP2P/MobileChatP2P/Chat.xaml.cs
+++ MobileChatP2P/MobileChatP2P/Chat.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,11 +13,30 @@ namespace MobileChatP2P
     public partial class Chat : ContentPage
     {
         public string meuIp { get; } = "192.168.0.0.1";
+        public ICommand enviarMensagem { get; }
         public Chat()
         {
             InitializeComponent();
 
             BindingContext = this;
+
+            enviarMensagem = new Command(_enviarMensagem);
+            button.Command = enviarMensagem;
+
         }
+
+        public void AddMensagem(View conteudo, Mensagem.Remetente remetente)
+        {
+            stack.Children.Add(new Mensagem(conteudo, remetente));
+        }
+
+        private void _enviarMensagem()
+        {
+            string msg = editor.Text;
+            Console.WriteLine(msg);
+            AddMensagem(new Label() { Text = msg }, Mensagem.Remetente.Servidor);
+        }
+
+
     }
 }
---
+++ MobileChatP2P/MobileChatP2P/Mensagem.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileChatP2P
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mensagem : ContentView
+    {
+        public enum Remetente
+        {
+            Cliente,
+            Servidor
+        }
+        public Mensagem(View conteudo, Remetente remetente)
+        {
+            InitializeComponent();
+            frame.Content = conteudo;
+            if(remetente == Remetente.Cliente)
+            {
+                frame.Margin = new Thickness(2, 2, 35, 2);
+            }
+        }
+
+    }
+}
