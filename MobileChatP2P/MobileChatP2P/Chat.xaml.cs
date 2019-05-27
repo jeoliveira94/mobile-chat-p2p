eb2332b730f2c51e0aa77657d2ec1a8a9eb7a1d7
José Eduardo de Souza Oliveira <eduardo.jeso@gmail.com>
2019-05-27 17:44:55 -0300

    commit inicial

---
+++ MobileChatP2P/MobileChatP2P/Chat.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileChatP2P
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Chat : ContentPage
+    {
+        public string meuIp { get; } = "192.168.0.0.1";
+        public Chat()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
