678521fec0f24f055da5064236981c6e9669db25
paulo mendes <paulomendes@nca.ufma.br>
2019-05-27 21:55:37 -0300

    Comunicacao client server

--- MobileChatP2P/MobileChatP2P/Chat.xaml.cs
+++ MobileChatP2P/MobileChatP2P/Chat.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,12 +12,35 @@ namespace MobileChatP2P
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Chat : ContentPage
     {
-        public string meuIp { get; } = "192.168.0.0.1";
+        public string meuIp { get; set; } = "192.168.0.0.1";
+        private SocketClient client;
+        private SocketServer server;
         public Chat()
         {
             InitializeComponent();
 
             BindingContext = this;
+
+            
+           
+
+            //client = new SocketClient();
+
+            //meuIp = client.myIpAddress.ToString();
+            RunServer();
+            //StartConnection("10.0.0.105");
+            //client.SendMessage("Eduardo eh doidao<EOF>");
+        }
+
+        private void StartConnection(string ipText)
+        {
+            client.StartClient(IPAddress.Parse(ipText));
+        }
+
+        public async void RunServer()
+        {
+            server = new SocketServer();
+            meuIp = await server.StartServer();
         }
     }
 }
---
+++ MobileChatP2P/MobileChatP2P/SocketClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MobileChatP2P
+{
+    public class SocketClient
+    {
+        Socket sender;
+        public IPAddress myIpAddress;
+
+        public SocketClient()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            this.myIpAddress = host.AddressList[0];
+
+            sender = new Socket(myIpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void SendMessage(string message) {
+            byte[] bytes = new byte[1024];
+
+            //byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+
+            // Send the data through the socket.    
+            int bytesSent = sender.Send(msg);
+
+            // Receive the response from the remote device.    
+            int bytesRec = sender.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+        }
+
+        public void StartClient(IPAddress serverIP)
+        {
+                                
+            IPEndPoint remoteEP = new IPEndPoint(serverIP, 11000);              
+                
+
+            // Connect the socket to the remote endpoint. Catch any errors.    
+            try
+            {
+                // Connect to Remote EndPoint  
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.ToString());
+            }
+        }
+
+        public void StopClient()
+        {
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
